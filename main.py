e8b4429b1a1dd3a257c7c5707cc2308c91cc238f
barbarosgursu-sudo <barbaros.gursu@gmail.com>
2026-02-24 11:58:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 import psycopg2
 import yfinance as yf
 import pandas as pd
-from datetime import datetime
 
 SYMBOLS = [
     "ASELS.IS",
@@ -17,6 +16,7 @@ def ingest_raw():
 
     for symbol in SYMBOLS:
         print(f"Fetching {symbol}")
+
         df = yf.download(
             symbol,
             period="5d",
@@ -38,17 +38,18 @@ def ingest_raw():
                 VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)
             """, (
                 symbol,
-                row["Datetime"],
-                row["Open"],
-                row["High"],
-                row["Low"],
-                row["Close"],
-                row.get("Adj Close", None),
-                row["Volume"],
+                row["Datetime"].to_pydatetime() if pd.notna(row["Datetime"]) else None,
+                float(row["Open"]) if pd.notna(row["Open"]) else None,
+                float(row["High"]) if pd.notna(row["High"]) else None,
+                float(row["Low"]) if pd.notna(row["Low"]) else None,
+                float(row["Close"]) if pd.notna(row["Close"]) else None,
+                float(row["Adj Close"]) if "Adj Close" in df.columns and pd.notna(row.get("Adj Close")) else None,
+                int(row["Volume"]) if pd.notna(row["Volume"]) else None,
                 "YAHOO"
             ))
 
         conn.commit()
+        print(f"{symbol} done")
 
     cur.close()
     conn.close()
